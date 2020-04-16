12d1e11d4dbbcbd89378395c4d6c361c38f1c5b2
huhrichard <richardpokard@gmail.com>
2020-04-16 16:16:41 -0400

    no message

--- performance_comparison.py
+++ performance_comparison.py
@@ -35,7 +35,7 @@ def extract_df_by_method(df, method='', drop_columns=['method']):
 def best_stacking_score(df, stacking_suffix='.S'):
 
     # return_df = pd.DataFrame([])
-    col_wo_method = df.columns.values.remove('method')
+    col_wo_method = df.columns.values.delete('method')
 
     pivoted_df = df.pivot_table('method', col_wo_method, 'fmax_stacking')
     pivoted_df = pivoted_df.reindex(col_wo_method+df['method'].unique())
