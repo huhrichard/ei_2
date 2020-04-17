3b0b63998a0cca5dc796a41cf22969bb914ec975
huhrichard <richardpokard@gmail.com>
2020-04-17 16:45:55 -0400

    no message

--- performance_comparison.py
+++ performance_comparison.py
@@ -164,6 +164,7 @@ if __name__ == "__main__":
     cd_input = ensemble_df_cat[['data_name','best_fmax','input']]
 
     cd_input_df = cd_input.pivot_table('best_fmax', ['data_name'],'input').reset_index()
+    cd_input_df.to_csv('cd_input_{}.csv'.format(group))
     print(cd_input_df)
     # print(ensemble_df_cat, ensemble_df_cat.columns)
     fig1 = plt.figure()
