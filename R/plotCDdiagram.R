7addf11ccadf8486f9893af1b583f5ff96ee2096
huhrichard <richardpokard@gmail.com>
2020-04-17 18:24:45 -0400

    no message

---
+++ R/plotCDdiagram.R
@@ -0,0 +1,21 @@
+library("scmamp")
+library("ggplot2")
+library("Rgraphviz")
+
+args=(commandArgs(TRUE))
+
+if(length(args)==0){
+    print("No arguments supplied.")
+    ##supply default values
+    cd_fn = "../cd_input.csv"
+}else{
+    for(i in 1:length(args)){
+         eval(parse(text=args[[i]]))
+    }
+}
+
+cd_input <- read.csv(file = cd_fn)
+
+png(file=gsub("csv", "png", cd_fn), ".png")
+plotCD(cd_input, alpha=0.05, cex=1.25)
+dev.off()
