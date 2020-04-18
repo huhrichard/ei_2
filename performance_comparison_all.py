f859c69ef92c29befcc5754f43647a2b7cf35697
huhrichard <richardpokard@gmail.com>
2020-04-17 21:57:16 -0400

    no message

--- performance_comparison_all.py
+++ performance_comparison_all.py
@@ -15,6 +15,6 @@ system('module load R')
 
 for o in list_ontology:
     for key, val in dict_to_compare.items():
-        cmd = python_cmd.format(key, val+o)
+        cmd = python_cmd.format(key, dir_prefix+val+o)
         print(cmd)
         system(cmd)
