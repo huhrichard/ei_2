6db9ccda3f25cd146ede08a09381d28c97e56cc0
huhrichard <richardpokard@gmail.com>
2020-04-23 11:08:09 -0400

    no message

--- performance_comparison_all.py
+++ performance_comparison_all.py
@@ -4,16 +4,16 @@ from os import system
 
 dir_prefix = '/sc/hydra/scratch/liy42/EIdata_'
 dict_to_compare = {
-                    # '1000': '1000_',
-                    # '500-1000': '500_1000_',
-                    # '200-500': '200_500_',
+                    '1000': '1000_',
+                    '500-1000': '500_1000_',
+                    '200-500': '200_500_',
                     '100-200': '100_200_',
                     '50-100': '50_100_',
                     }
 #
 list_ontology = [
-                 'go',
-                 # 'hpo'
+                 # 'go',
+                 'hpo'
                 ]
 python_cmd = 'python performance_comparison.py {} {}'
 system('module load R')
