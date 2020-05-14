067470607a28b1e219796c66a5f937034cd98581
huhrichard <richardpokard@gmail.com>
2020-05-13 20:08:49 -0400

    no message

--- plot_from_cd_input.py
+++ plot_from_cd_input.py
@@ -121,7 +121,9 @@ for ontology in list_ontology:
         ax1 = fig1.add_subplot(111)
         ax1 = sns.boxplot(ax=ax1, y='fmax', x='algo',
                           data=cd_df_melted, palette=sorted_cp, order=sorted_algo_names)
+
         for tick in ax1.get_xticklabels():
+            tick.label.set_fontsize(16)
             tick.set_rotation(45)
             tick.set_horizontalalignment("right")
         ax1.set_ylabel(ylabel)
