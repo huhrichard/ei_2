4335586d3e539e3fd4bb896eccc14f588765a6a0
huhrichard <richardpokard@gmail.com>
2020-05-27 16:13:10 -0400

    no message

--- submit_all_covid19.py
+++ submit_all_covid19.py
@@ -9,6 +9,7 @@ list_of_method = ['EI', 'demographics',
                   'vitals', 'concatenated',
                   'EI_svdImpute', 'EI_svdImpute_rank_5', 'EI_svdImpute_rank_20',
                   'concatenated_svdImpute', 'concatenated_svdImpute_rank_5', 'concatenated_svdImpute_rank_20',
+                  'labs_svdImpute', 'labs_svdImpute_rank_5', 'labs_svdImpute_rank_20'
                   ]
 
 outcome_list = ['DECEASED_INDICATOR']
