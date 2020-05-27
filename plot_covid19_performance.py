e625db8689b3875fdcb0a0e569da108896f5091b
huhrichard <richardpokard@gmail.com>
2020-05-27 17:06:19 -0400

    no message

--- plot_covid19_performance.py
+++ plot_covid19_performance.py
@@ -7,7 +7,12 @@ import seaborn as sns
 base_path = '/sc/hydra/scratch/liy42/covid19_DECEASED_INDICATOR/'
 
 list_of_method = ['EI', 'demographics',
-                  'labs_type', 'medications_frequency', 'vitals_numeric', 'concatenated']
+                  'labs', 'medications',
+                  'vitals', 'concatenated',
+                  'EI_svdImpute', 'EI_svdImpute_rank_5', 'EI_svdImpute_rank_20',
+                  'concatenated_svdImpute', 'concatenated_svdImpute_rank_5', 'concatenated_svdImpute_rank_20',
+                  'labs_svdImpute', 'labs_svdImpute_rank_5', 'labs_svdImpute_rank_20'
+                  ]
 
 outcome_list = ['DECEASED_INDICATOR']
 
@@ -20,10 +25,20 @@ path_of_performance = '/analysis/performance.csv'
 
 dict_of_method = {'EI':'Ensemble\nIntegration',
                   'demographics':'Demographics',
-                  'labs_type':'Labs',
-                  'medications_frequency': 'Medications',
-                  'vitals_numeric': 'Vitals',
-                  'concatenated': 'Concantenated\nAll'}
+                  'labs':'Labs',
+                  'medications': 'Medications',
+                  'vitals': 'Vitals',
+                  'concatenated': 'Concantenated\nAll',
+                  'EI_svdImpute': 'Ensemble Integration\nSVDImpute',
+                  'EI_svdImpute_rank_5':'Ensemble Integration\nSVDImpute(rank=5)',
+                  'EI_svdImpute_rank_20': 'Ensemble Integration\nSVDImpute(rank=20)',
+                  'concatenated_svdImpute': 'Concantenated All\nSVDImpute',
+                  'concatenated_svdImpute_rank_5': 'Concantenated All\nSVDImpute(rank=5)',
+                  'concatenated_svdImpute_rank_20': 'Concantenated All\nSVDImpute(rank=20)',
+                  'labs_svdImpute': 'Labs\nSVDImpute',
+                  'labs_svdImpute_rank_5':'Labs\nSVDImpute(rank=5)',
+                  'labs_svdImpute_rank_20': 'Labs\nSVDImpute(rank=20)'
+                  }
 cp = sns.color_palette(n_colors=len(dict_of_method))
 dict_suffix = [v for k, v in dict_of_method.items()]
 fmax_median_list = []
@@ -60,7 +75,7 @@ def custom_boxplot(boxplot_y_metric, boxplot_ylabel, sorted_algo_names, sorted_c
     for tick in ax1.get_xticklabels():
         tick.set_fontsize(16)
         tick.set_rotation(45)
-        tick.set_horizontalalignment("right")
+        tick.set_horizontalalignment("center")
 
     ax1.set_title('COVID-19 Deceased Prediction')
     fig1.savefig('{}covid19_{}_comparison.png'.format(plot_dir, boxplot_y_metric), bbox_inches="tight")
