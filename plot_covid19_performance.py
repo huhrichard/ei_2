deb34bee4ea4ef3a6a89bffa3c1d3f75c98fef4a
huhrichard <richardpokard@gmail.com>
2020-05-28 21:40:20 -0400

    no message

--- plot_covid19_performance.py
+++ plot_covid19_performance.py
@@ -71,7 +71,7 @@ def plot_boxplot_fmax_auc(list_of_method, fig_fn_suffix):
     print(sorted_tuple_by_auc)
     fmax_label = r'$F_{max}$'
     def custom_boxplot(boxplot_y_metric, boxplot_ylabel, sorted_algo_names, sorted_cp):
-        fig1, ax1 = plt.subplots(1,1, figsize=(9,6))
+        fig1, ax1 = plt.subplots(1,1, figsize=(7,6))
         ax1 = sns.boxplot(ax=ax1, y=boxplot_y_metric, x='data_name', data=performance_cat_df,
                           palette=sorted_cp, order=sorted_algo_names)
         ax1.set_ylabel(boxplot_ylabel, fontsize=22)
