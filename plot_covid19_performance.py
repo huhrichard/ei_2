bf5f637c1484d5696368479ca31b4b191edb9d97
huhrichard <richardpokard@gmail.com>
2020-05-28 21:55:00 -0400

    no message

--- plot_covid19_performance.py
+++ plot_covid19_performance.py
@@ -77,7 +77,7 @@ def plot_boxplot_fmax_auc(list_of_method, fig_fn_suffix):
         ax1.set_ylabel(boxplot_ylabel, fontsize=22)
 
         for tick in ax1.get_xticklabels():
-            # tick.set_fontsize(12)
+            tick.set_fontsize(12)
             tick.set_rotation(45)
             tick.set_fontweight('bold')
             tick.set_horizontalalignment("right")
