f67b0b52e8ad933dfe1d6ed6044e9952a66703fa
huhrichard <richardpokard@gmail.com>
2020-06-05 15:58:48 -0400

    no message

--- generate_term_jobs.py
+++ generate_term_jobs.py
@@ -46,8 +46,8 @@ go_pos_count = sum(pos_entry)
 # suffix = '_experimental'
 # suffix = ''
 dict_suffix = {'': 'EI',
-               # 'deepNF': 'DeepNF',
-               # 'mashup': 'Mashup',
+               'deepNF': 'DeepNF',
+               'mashup': 'Mashup',
                'coexpression': 'Coexpression',
                'cooccurence': 'Cooccurence',
                # 'database': 'Database',
@@ -67,11 +67,11 @@ for suffix, val in dict_suffix.items():
     go_by_count_dict = {
                         'EIdata_500_1000_{}.jobs'.format(ontology_suffix):np.logical_and((go_pos_count>500), (go_pos_count<=1000)),
                         'EIdata_1000_{}.jobs'.format(ontology_suffix): go_pos_count > 1000,
-                        'EIdata_200_500_{}.jobs'.format(ontology_suffix): np.logical_and((go_pos_count>200), (go_pos_count<=500)),
-                        'EIdata_10_50_{}.jobs'.format(ontology_suffix): np.logical_and((go_pos_count>10), (go_pos_count<=50)),
-                        'EIdata_10_{}.jobs'.format(ontology_suffix): go_pos_count<=10,
-                        'EIdata_50_100_{}.jobs'.format(ontology_suffix): np.logical_and((go_pos_count>50), (go_pos_count<=100)),
-                        'EIdata_100_200_{}.jobs'.format(ontology_suffix): np.logical_and((go_pos_count>100), (go_pos_count<=200)),
+                        # 'EIdata_200_500_{}.jobs'.format(ontology_suffix): np.logical_and((go_pos_count>200), (go_pos_count<=500)),
+                        # 'EIdata_10_50_{}.jobs'.format(ontology_suffix): np.logical_and((go_pos_count>10), (go_pos_count<=50)),
+                        # 'EIdata_10_{}.jobs'.format(ontology_suffix): go_pos_count<=10,
+                        # 'EIdata_50_100_{}.jobs'.format(ontology_suffix): np.logical_and((go_pos_count>50), (go_pos_count<=100)),
+                        # 'EIdata_100_200_{}.jobs'.format(ontology_suffix): np.logical_and((go_pos_count>100), (go_pos_count<=200)),
                         }
 
     IC_list = []
