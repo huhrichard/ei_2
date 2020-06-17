759bf0f4d98c04249d4d922e7996aa18e6e214d7
huhrichard <richardpokard@gmail.com>
2020-06-17 12:04:45 -0400

    no message

--- xgboost_feat_importance.py
+++ xgboost_feat_importance.py
@@ -65,7 +65,7 @@ print(f_max)
 feat_imp_df = pd.DataFrame(data=feat_imp, columns=X_cols)
 # feat_imp_df = feat_imp_df.stack()
 feat_imp_median = feat_imp_df.median(axis=0)
-feat_imp_median.sort_values(inplace=True)
+feat_imp_median.sort_values(inplace=True, ascending=False)
 
 
 feat_imp_df['idx'] = feat_imp_df.index
