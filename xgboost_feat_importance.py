4fc02a7f25c0caa26cd87b0963b1370d7ea571ee
huhrichard <richardpokard@gmail.com>
2020-06-16 20:01:00 -0400

    no message

---
+++ xgboost_feat_importance.py
@@ -0,0 +1,62 @@
+import xgboost
+import pandas as pd
+from sklearn.model_selection import KFold
+import numpy as np
+from pandas.api.types import is_string_dtype
+# from ... import common
+from fancyimpute import IterativeSVD
+
+# f_path = 'msdw_covid19_27May2020'
+f_path = '.'
+concat_df = pd.read_csv('{}/{}'.format(f_path, 'concatenated.csv'), index_col=0)
+outcome_df = pd.read_csv('{}/{}'.format(f_path, 'outcome.csv'), index_col=0)
+
+concat_dtype = concat_df.dtypes
+
+# print(concat_dtype)
+
+for c, dtype in concat_dtype.items():
+    # print(c, dtype)
+    if is_string_dtype(dtype):
+        concat_df = pd.concat([concat_df, pd.get_dummies(concat_df[c], prefix=c, dummy_na=True)],axis=1)
+        # print(concat_df.columns)
+        concat_df.drop(columns=[c], inplace=True)
+        # print(concat_df.columns)
+
+concat_df = concat_df.apply(pd.to_numeric)
+concat_df=(concat_df-concat_df.mean())/concat_df.std()
+concat_df[:] = IterativeSVD().fit_transform(concat_df.values)
+
+data_outcome_df = pd.concat([concat_df, outcome_df], axis=1, join='inner')
+
+
+
+
+
+kf = KFold(n_splits=5, shuffle=True, random_state=1)
+outcome = 'DECEASED_INDICATOR'
+y = data_outcome_df[outcome].values.astype(float)
+X = data_outcome_df.drop(columns=[outcome]).values.astype(float)
+feat_imp_list = []
+test_pred_list = []
+test_label_list = []
+
+
+print(X.shape)
+for train_idx, test_idx in kf.split(X):
+    X_train, X_test = X[train_idx], X[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
+
+    clf = xgboost.XGBClassifier(missing=np.nan)
+    clf.fit(X_train, y_train)
+    feat_imp_list.append(clf.feature_importances_)
+    test_pred_list.append(clf.predict_proba(X_test)[:,1])
+    test_label_list.append(y_test)
+
+feat_imp = np.array(feat_imp_list)
+test_pred = np.concatenate(test_pred_list)
+test_label = np.concatenate(test_label_list)
+
+print(feat_imp.shape)
+print(test_pred.shape)
+print(test_label.shape)
