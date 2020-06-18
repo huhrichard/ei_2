9a28cc731a32faeb119a72857a1f98549a4c8bb5
huhrichard <richardpokard@gmail.com>
2020-06-18 17:47:18 -0400

    no message

--- plot_covid19_performance.py
+++ plot_covid19_performance.py
@@ -105,7 +105,7 @@ def plot_boxplot_fmax_auc(list_of_method, fig_fn_suffix, base_path_tuple):
             auc_median_list.append(df['auc'].median())
 
     performance_cat_df = pd.concat(performance_df_list)
-    print(df['data_name'])
+    print(performance_cat_df['data_name'])
     fmax_label = r'$F_{max}$'
     def custom_boxplot(boxplot_y_metric, boxplot_ylabel, metric_median_list):
         sorted_tuple = sorted(zip(metric_median_list, dict_suffix, cp), reverse=True, key=lambda x: x[0])
