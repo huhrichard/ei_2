8b6ef65235396285b188ff4fd4787ab9e1b237aa
huhrichard <richardpokard@gmail.com>
2020-06-25 10:23:54 -0400

    no message

--- performance_comparison.py
+++ performance_comparison.py
@@ -29,6 +29,7 @@ def find(pattern, path):
     result = []
     for root, dirs, files in os.walk(path):
         for name in files:
+            print(name, files)
             if fnmatch.fnmatch(name, pattern):
                 result.append(os.path.join(root, name))
     return result
