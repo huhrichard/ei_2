7cc1f19ed058c10ca2c8b0551f9215914be753f4
huhrichard <richardpokard@gmail.com>
2020-08-06 17:24:22 -0400

    no message

--- plot_covid19_performance.py
+++ plot_covid19_performance.py
@@ -186,6 +186,8 @@ def plot_boxplot_fmax_auc(list_of_method, fig_fn_suffix, base_path_tuple):
         ax2 = sns.heatmap(ax=ax2,
                           data=pivoted,
                           annot=True, fmt='.3f')
+
+        ax2.set_yticklabels(ax2.get_yticklabels(), rotation=-90)
         fig2.savefig('{}covid19_{}_{}_heatmap_{}.tif'.format(plot_dir, boxplot_y_metric, fig_fn_suffix, exp_name),
                      bbox_inches="tight",
                      pil_kwargs={"compression": "tiff_lzw"}
