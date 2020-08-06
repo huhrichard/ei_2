3b026cac00e0d0fe597fb2ad8f8cc19ec62614c2
huhrichard <richardpokard@gmail.com>
2020-08-06 17:16:03 -0400

    no message

--- plot_covid19_performance.py
+++ plot_covid19_performance.py
@@ -178,6 +178,10 @@ def plot_boxplot_fmax_auc(list_of_method, fig_fn_suffix, base_path_tuple):
 
         fig2, ax2 = plt.subplots(1, 1, figsize=(10, 10))
         pivoted = performance_cat_df.pivot("data_name", "method", boxplot_y_metric)
+        pivoted.reindex(sorted_algo_names, inplace=True)
+        pivoted.reindex(pivoted.median().sort_values().index, axis=1)
+        # pivoted.assign(m=pivoted.median(axis=1)).sort_values('m').drop('m', axis=1)
+
         print(pivoted)
         ax2 = sns.heatmap(ax=ax2,
                           data=pivoted,
