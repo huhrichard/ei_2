87b8b703d7c665b194ebc1b7a09322d2ccd39522
huhrichard <richardpokard@gmail.com>
2020-09-09 13:07:42 -0400

    no message

--- generate_data.py
+++ generate_data.py
@@ -94,7 +94,7 @@ def processTermFeature_3(param, impute, fold=5):
     for fold_attr, (kf_train_idx, kf_test_idx) in enumerate(kf_idx_list):
         # f_bool = np.zeros(merged_df.shape[0], int)
         # f_bool[kf_test_idx] = 1
-        merged_df.iloc[kf_test_idx, 'fold'] = fold_attr
+        merged_df.iloc[kf_test_idx].loc[:,'fold'] = fold_attr
 
     print('after', merged_df.shape)
     # del merged_df.index.name
