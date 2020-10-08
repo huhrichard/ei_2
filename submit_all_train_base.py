ba3a5cd7d832f77c86ae880a16be1f4869239196
huhrichard <richardpokard@gmail.com>
2020-10-08 12:16:16 -0400

    no message

--- submit_all_train_base.py
+++ submit_all_train_base.py
@@ -1,10 +1,14 @@
 import sys, os
 
 ontology = sys.argv[-1]
+queue_n = 'express'
 lsf_fn = 'train_all_base_{}.lsf'.format(ontology)
 script = open(lsf_fn, 'w')
-script.write('#!/bin/bash\n#BSUB -J train_all_base\n#BSUB -P acc_pandeg01a\n#BSUB -q express\n#BSUB -n 4\n#BSUB -W 10:00\n#BSUB -o train_all_base.stdout\n#BSUB -eo train_all_base.stderr\n#BSUB -R rusage[mem=10000]\n')
-script.write('module purge\nmodule load java\nmodule load python\nmodule load groovy\nmodule load selfsched\n')
+script.write('#!/bin/bash\n#BSUB -J train_all_base\n#BSUB -P acc_pandeg01a\n#BSUB -q {}\n'
+             '#BSUB -n 4\n#BSUB -W 10:00\n#BSUB -o train_all_base.stdout\n'
+             '#BSUB -eo train_all_base.stderr\n#BSUB -R rusage[mem=10000]\n'.format(queue_n))
+script.write('module purge\nmodule load java\nmodule load python\nmodule load groovy\n'
+             'module load selfsched\n')
 
 python_cmd = 'python run_all_different_data_train_base.py {}'.format(ontology)
 script.write(python_cmd)
