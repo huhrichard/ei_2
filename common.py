8397d518f9de3f202a17e317f8246ad6bf6c417e
huhrichard <richardpokard@gmail.com>
2020-10-29 11:00:43 -0400

    no message

--- common.py
+++ common.py
@@ -43,6 +43,7 @@ def fmeasure_score(labels, predictions, thres=None, beta = 1.0, pos_label = 1):
     if thres is None:
         precision, recall, threshold = sklearn.metrics.precision_recall_curve(labels, predictions, pos_label)
         f1 = (1 + beta**2) * (precision * recall) / ((beta**2 * precision) + recall)
+        print(threshold)
         return {'F':nanmax(f1), 'thres':threshold[where(f1==nanmax(f1))][0]}
 
     else:
