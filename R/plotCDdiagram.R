7ee7ff75fedab7fdb44bc51f41a50eeac773a9ba
huhrichard <richardpokard@gmail.com>
2020-11-11 18:53:53 -0500

    no message

--- R/plotCDdiagram.R
+++ R/plotCDdiagram.R
@@ -18,6 +18,7 @@ if(length(args)==0){
 cd_input <- read.csv(file = cd_fn)
 cd_input <- na.omit(cd_input)
 fn <- gsub("csv", "pdf", cd_fn)
+print(cd_input)
 
 # fn <- gsub("cd_input", "./plot/cd_input", fn)
 prefix = './plot/'
@@ -29,8 +30,7 @@ pdf(file=fn, width=15, bg="white")
 par(mar=c(1,5,1,5))
 cdplot <- plotCD(cd_input, alpha=0.05, cex=0.9)  +
   geom_point() +
-  coord_fixed(ratio = 0.5) +
-    coord_flip(clip = "off")
+  coord_fixed(ratio = 0.5) + coord_flip(clip = "off")
 # ggsave(filename=gsub("csv", "png", cd_fn), plot=cdplot)
 dev.off()
 
