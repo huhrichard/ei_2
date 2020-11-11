3f31d1106abd5e59300065eb12adeca03f6a33b2
huhrichard <richardpokard@gmail.com>
2020-11-11 15:14:25 -0500

    no message

--- check_missing_value.py
+++ check_missing_value.py
@@ -21,20 +21,31 @@ edge_txt_format = 'human_string_{}_adjacency.txt'
 csv_path = 'not_on_github/csv/'
 features = ['coexpression', 'cooccurence', 'database', 'experimental', 'fusion', 'neighborhood']
 
+nz_rows_list = []
 for f in features:
     df = pd.read_csv('{}{}.csv'.format(csv_path, f), index_col=0)
     print(f, 'original shape:', df.shape)
 
     np_a = df.values
-    print(np_a)
-    zero_rows = (np.all(np_a == 0, axis=0))
-    zero_cols = (np.all(np_a == 0, axis=1))
-    print(np.sum(zero_rows))
-    print(np.sum(zero_cols))
-    zero_rows_df = (df == 0).all(axis=0)
-    zero_cols_df = (df == 0).all(axis=1)
-    print(np.sum(zero_rows_df))
-    print(np.sum(zero_cols_df))
+    # print(np_a)
+    # zero_rows = (np.all(np_a == 0, axis=0))
+    zero_rows = (np.all(np_a == 0, axis=1))
+    nz_rows_list.append(~zero_rows)
+    # print(np.sum(zero_rows))
+    # print(np.sum(zero_cols))
+    # zero_rows_df = (df == 0).all(axis=0)
+    # zero_cols_df = (df == 0).all(axis=1)
+    # print(np.sum(zero_rows_df))
+    # print(np.sum(zero_cols_df))
     # print(np_a == 0)
     # print('{} missing count: {}%'.format(f, sum(df.isnull().values)))
+
+nz_all = np.ones_like(nz_rows_list[0])
+
+for nz_rows in nz_rows_list:
+    nz_all = np.logical_and(nz_all, nz_rows)
+
+print(np.sum(nz_all))
+
+
 
