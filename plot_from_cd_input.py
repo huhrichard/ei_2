1ff9bc148035e6064e70ff532b1fc41dae1b7f22
huhrichard <richardpokard@gmail.com>
2020-11-11 21:07:54 -0500

    no message

--- plot_from_cd_input.py
+++ plot_from_cd_input.py
@@ -205,7 +205,7 @@ for ontology in list_ontology:
             print(new_tick)
             tick.set_text(new_tick)
             ax2_new_xticks.append(new_tick)
-            tick.set_fontsize(21)
+            tick.set_fontsize(20.5)
 
         ax2.set_xticklabels(ax2_new_xticks)
 
@@ -250,7 +250,7 @@ for ontology in list_ontology:
         ax3.set_ylabel(ylabel)
         for tick in ax3.get_xticklabels():
 
-            tick.set_fontsize(21)
+            tick.set_fontsize(20.5)
         ax3.set_xlabel('Information Content', fontsize=21)
         # ax3.set_title(title_name)
         # fig3.savefig('{}f_max_{}_by_ic_{}.png'.format(plot_dir, ontology, group_fn_suffix), bbox_inches="tight")
