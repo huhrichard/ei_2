2b22884e4b9cb9e3ab7e928b4cf3bce9f9d3a877
Richard <richardpokard@gmail.com>
2021-06-15 16:14:08 -0400

    no message

--- xgboost_prediction.py
+++ xgboost_prediction.py
@@ -83,10 +83,7 @@ def xgboost_predictions_result(outcome_path):
 
         train_label = train_nf[label_col]
 
-        xgb_clf = XGBClassifier(n_jobs=-1,
-                                # gpu_id=0,
-                                n_estimators=80,
-                        max_delta_step=1)
+        xgb_clf = XGBClassifier()
         xgb_clf.fit(train_feat, train_label)
         test_prediction = xgb_clf.predict_proba(test_feat)[:, 1]
         # test_predictions.append(test_prediction)
