aee407549da4807b1670758babd214bacadb170a
Richard <richardpokard@gmail.com>
2021-09-22 13:59:21 -0400

    no message

--- ensemble_ranking.py
+++ ensemble_ranking.py
@@ -51,15 +51,15 @@ print(imp_base_features)
 
 multiplied_rank_col = 'bfxbp_rank'
 
-imp_base_predictors['bp_rank'] = imp_base_predictors[bpdf_imp_col].rank(pct=True)
-imp_base_features['bf_rank'] = imp_base_features[bfdf_imp_col].rank(pct=True)
+imp_base_predictors['bp_descending_rank'] = imp_base_predictors[bpdf_imp_col].rank(pct=True, ascending=False)
+imp_base_features['bf_descending_rank'] = imp_base_features[bfdf_imp_col].rank(pct=True, ascending=False)
 imp_base_features[multiplied_rank_col] = 0.0
 
 for bp_idx, bp in imp_base_predictors.iterrows():
     bp_name = bp[bp_name_col_bpdf]
-    bp_rank = bp['bp_rank']
+    bp_rank = bp['bp_descending_rank']
     bf_df_matched_bool = imp_base_features[bp_name_col_bfdf] == bp_name
-    bf_ranks = imp_base_features.loc[bf_df_matched_bool, 'bf_rank']
+    bf_ranks = imp_base_features.loc[bf_df_matched_bool, 'bf_descending_rank']
     print(bf_ranks*bp_rank)
     imp_base_features.loc[bf_df_matched_bool, multiplied_rank_col] = bf_ranks*bp_rank
 
@@ -83,15 +83,15 @@ for base_feature in base_features_list:
     base_feature_rank_median.append(median_rank)
     # base_feature_rank_agg['min_agg'] = [avg_ranks]
 
-base_feature_rank_agg['min'] = base_feature_rank_min
-base_feature_rank_agg['mean'] = base_feature_rank_mean
-base_feature_rank_agg['median'] = base_feature_rank_median
+base_feature_rank_agg['min_agg'] = base_feature_rank_min
+base_feature_rank_agg['mean_agg'] = base_feature_rank_mean
+base_feature_rank_agg['median_agg'] = base_feature_rank_median
 
 
 base_feature_rank_df = pd.DataFrame(base_feature_rank_agg, index=base_features_list)
-base_feature_rank_df['min_rank'] = base_feature_rank_df['min'].rank()
-base_feature_rank_df['mean_rank'] = base_feature_rank_df['mean'].rank()
-base_feature_rank_df['median_rank'] = base_feature_rank_df['median'].rank()
+base_feature_rank_df['min_accending_rank'] = base_feature_rank_df['min_agg'].rank()
+base_feature_rank_df['mean_accending_rank'] = base_feature_rank_df['mean_agg'].rank()
+base_feature_rank_df['median_accending_rank'] = base_feature_rank_df['median_agg'].rank()
 # base_feature_rank_df['min_rank'] = base_feature_rank_df[].rank()
 # base_feature_rank_df.rename(columns={imp_base_predictors.columns[0]: 'bf_rank'}, inplace=True)
 base_feature_rank_df.to_csv('base_feature_agg_rank.csv')
