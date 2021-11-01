942dd5919b3d88cb367c29939241dbaf0069ec72
Richard <richardpokard@gmail.com>
2021-11-01 10:41:53 -0400

    no message

--- filling_jobs.py
+++ filling_jobs.py
@@ -4,19 +4,20 @@ import sys
 term = sys.argv[-2]
 annotation = sys.argv[-1]
 
-cd_df = pd.read_csv('./plot/cd_csv/cd_input_{}_{}.csv'.format(term, annotation))
+cd_df = pd.read_csv('./plot/cd_csv/cd_input_{}_{}_fmax.csv'.format(term, annotation))
 
 annotation_understroke = annotation.replace('-', '_')
 
-data = 'EI'
+data_list = ['EI', 'mashup', 'deepNF']
 
-fn = 'EIdata_{}_{}_{}.jobs'.format(annotation_understroke, term, data)
-jobs_fn = './jobs/'+fn
-f = open(jobs_fn, 'w')
+for data in data_list:
+    fn = 'EIdata_{}_{}_{}.jobs'.format(annotation_understroke, term, data)
+    jobs_fn = './jobs/'+fn
+    f = open(jobs_fn, 'w')
 
-missed_term = cd_df[cd_df.isnull().any(axis=1)].index
-print(missed_term)
-cmd_str = 'python generate_data.py {} {}/ {}\n'
-for t in missed_term:
-    f.write(cmd_str.format(t, fn.split('.')[0], data))
-f.close()
+    missed_term = cd_df[cd_df.isnull().any(axis=1)].index
+    print(missed_term)
+    cmd_str = 'python generate_data.py {} {}/ {}\n'
+    for t in missed_term:
+        f.write(cmd_str.format(t, fn.split('.')[0], data))
+    f.close()
