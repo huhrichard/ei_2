476b19a76162352210da118b2247326e4fde1056
Richard <richardpokard@gmail.com>
2021-11-17 08:26:02 -0500

    no message

--- generate_data.py
+++ generate_data.py
@@ -67,7 +67,7 @@ def convert_to_arff(df, path):
 # #	arff.dump(path, feature_df.values, relation='linhuaw', names = feature_df.columns)
 #         convert_to_arff(feature_df,path)
 
-def processTermFeature_3(param, impute, fold=5):
+def TermFeature_index(param, impute, fold=5):
     term, feature, go_hpo_df, csv_filepath = param
     if impute:
         feature_df = pd.read_csv('{}rwrImputed_{}.csv'.format(csv_filepath, feature), index_col=0)
@@ -86,14 +86,32 @@ def processTermFeature_3(param, impute, fold=5):
     filled_df = feature_df.fillna(0)
     cols = (filled_df == 0).all(axis=0)
     cols = cols.loc[cols == False].index.tolist()
-    filled_df = filled_df[cols]
-    filled_df = filled_df.round(3)
+
+    return cols
+
+def preprocessingTermFeat(param, impute, index_list, fold=5):
+    term, feature, go_hpo_df, csv_filepath = param
+    if impute:
+        feature_df = pd.read_csv('{}rwrImputed_{}.csv'.format(csv_filepath, feature), index_col=0)
+        print('using imputed data')
+    else:
+        feature_df = pd.read_csv('{}{}.csv'.format(csv_filepath, feature), index_col=0)
+
+    before_shape = feature_df.shape
+    go_hpo_df.fillna(0, inplace=True)
+    go_hpo_df = go_hpo_df[go_hpo_df != 0]
+    go_hpo_df.replace(-1, 'neg', inplace=True)
+    go_hpo_df.replace(1, 'pos', inplace=True)
+
+    filled_df = feature_df.fillna(0)
+    filled_df = filled_df[index_list]
+    filled_df = filled_df.round(5)
     # merged_df = pd.merge(filled_df, go_hpo_df, how='inner')
     merged_df = pd.concat([filled_df, go_hpo_df], axis=1, join='inner')
     merged_df.rename(columns={term: 'cls'}, inplace=True)
     merged_df['seqID'] = merged_df.index
     print('before', merged_df.shape)
-    # merged_df.dropna(inplace=True)
+    merged_df.dropna(inplace=True)
     kf_split = StratifiedKFold(n_splits=fold, shuffle=True, random_state=64)
     # kf_split = KFold(n_splits=fold, shuffle=True, random_state=64)
     kf_idx_list = kf_split.split(merged_df, y=merged_df['cls'])
@@ -238,8 +256,19 @@ if __name__ == "__main__":
     print(
         '[STARTED: %s] start multithreads computing to generate feature files for GO term: %s...............................' % (
         str(datetime.datetime.now()), term))
+    col_indices = []
     for param in params:
-        processTermFeature_3(param, impute=impute_graph, fold=fold)
+        cols = TermFeature_index(param, impute=impute_graph, fold=fold)
+        col_indices.append(cols)
+
+    col_intersect = col_indices[0]
+    for col_index in col_indices:
+        col_intersect = np.logical_and(col_index, col_intersect)
+
+    for param in params:
+        cols = preprocessingTermFeat(param, impute=impute_graph, index_list=col_intersect, fold=fold)
+
+
 
     # p = Pool(6)
 
