e1828f69e0518071b098b640989f5628032c0899
Richard <richardpokard@gmail.com>
2021-12-17 14:11:04 -0500

    no message

--- performance_comparison_covid.py
+++ performance_comparison_covid.py
@@ -367,7 +367,7 @@ if __name__ == "__main__":
         sorted_dataname_dict = {s:s.replace('-\n', '').replace('\n',' ') for s in sorted_dataname_list}
         sorted_dataname_list = [v for k,v in sorted_dataname_dict.items()]
         ensemble_df_cat.replace(sorted_dataname_dict, inplace=True)
-        fig2, ax2 = plt.subplots(1, 1, figsize=(18, 6))
+        fig2, ax2 = plt.subplots(1, 1, figsize=(20, 6))
         ax2 = sns.barplot(ax=ax2, y=best_metric_str, x='Outcome', hue='Method',
                           hue_order=sorted_dataname_list,
                           data=ensemble_df_cat, palette=sorted_cp,
