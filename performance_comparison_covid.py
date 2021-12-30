2b1c142525ec75ebd7414a3d28656480ab7683e5
Richard <richardpokard@gmail.com>
2021-12-30 10:29:22 -0500

    no message

--- performance_comparison_covid.py
+++ performance_comparison_covid.py
@@ -374,7 +374,7 @@ if __name__ == "__main__":
                           data=ensemble_df_cat, palette=sorted_cp,
                           order=outcomes_order,
                           )
-        ax2.legend(mode = "expand", ncol=np.floor(len(sorted_dataname_list)/2), fontsize=20)
+        ax2.legend(mode = "expand", ncol=int(np.floor(len(sorted_dataname_list)/2)), fontsize=20)
         # for tick in ax3.get_xticklabels():
         #     tick.set_rotation(45)
         #     tick.set_horizontalalignment("right")
@@ -449,7 +449,7 @@ if __name__ == "__main__":
                           data=performance_df_cat_noxgb, palette=sorted_cp_no_xgb,
                           order=outcomes_order,
                           )
-        ax2.legend(mode="expand", ncol=np.floor(len(sorted_dataname_list)/2))
+        ax2.legend(mode="expand", ncol=int(np.floor(len(sorted_dataname_list)/2)), fontsize=20)
         # for tick in ax3.get_xticklabels():
         #     tick.set_rotation(45)
         #     tick.set_horizontalalignment("right")
