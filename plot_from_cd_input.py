e0ded15bad25adb2dfb9dacc989ba08eaf1f863c
Richard <richardpokard@gmail.com>
2022-01-07 07:39:32 -0500

    no message

--- plot_from_cd_input.py
+++ plot_from_cd_input.py
@@ -39,7 +39,7 @@ def find(pattern, path):
     return result
 
 dict_suffix = ['Ensemble\nIntegration',
-                'DeepNF',
+                'deepNF',
                 'Mashup',
                 'Coexpression',
                 'Co-occurrence',
@@ -54,7 +54,7 @@ godag = get_godag("go-basic.obo")
 objanno = get_objanno('goa_human.gaf', 'gaf', godag=godag)
 termcounts = TermCounts(godag, objanno.get_id2gos_nss())
 
-cd_csv_path = 'plot/cd_csv/'
+cd_csv_path = 'plot/'
 # list_ontology = ['go', 'hpo']
 list_ontology = ['go']
 # list_of_groups = ['1000', '500-1000', '200-500', '100-200', '50-100', '10-50']
