dd2e287df7a05ba7b9f2974286270932912f0af7
Richard <richardpokard@gmail.com>
2022-01-17 12:56:52 -0500

    no message

--- train_base.py
+++ train_base.py
@@ -107,8 +107,8 @@ if __name__ == "__main__":
         if not exists(feature_rank_path):
             os.mkdir(feature_rank_path)
         data_path, feature_folders = create_pseudoTestdata(feature_rank_path,
-    #                                                        feature_folders,
-    #                                                        original_dir=data_path)
+                                                           feature_folders,
+                                                           original_dir=data_path)
 
 
     # ### get paths of the list of features
@@ -122,7 +122,7 @@ if __name__ == "__main__":
 
 
 
-    assert len(feature_folders) > 0
+    # assert len(feature_folders) > 0
 
     # get fold, id and label attribute
 
