00171172246b01211c895ec22c715a20c287bb18
Richard <richardpokard@gmail.com>
2022-02-08 20:27:16 -0500

    no message

--- convert_deepnf_data.py
+++ convert_deepnf_data.py
@@ -20,10 +20,19 @@ if __name__ == "__main__":
 
     G = nx.read_edgelist(os.path.join(args.deepnf_path, 'coexpression_deepnf.txt'), edgetype=float, data=(('weight', float),))
     node_list = list(G.nodes())
+    print(node_list)
+    print(len(node_list))
     protein_list = pd.read_csv(os.path.join(args.deepnf_path, 'gene_list.txt'))
+    print(protein_list.shape)
 
     with open(args.pkl_path, 'rb') as handle:
         b = pickle.load(handle)
-    print(b)
+    print(b.shape)
     print(type(b))
+    deepnf_cols = ['dnf_{}'.format(i) for i in b.shape[1]]
+    deepnf_df = pd.DataFrame(b,
+                             index=protein_list.values,
+                             columns=deepnf_cols)
+    deepnf_df.to_csv(os.path.join(args.output_path, 'deepNF.csv'))
+
 
