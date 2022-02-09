f49c46dbd03534f4bea12dadda35d5c97a7198a8
Richard <richardpokard@gmail.com>
2022-02-08 19:16:52 -0500

    no message

--- convert_deepnf_data.py
+++ convert_deepnf_data.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--deepnf_path', type=str, required=True, help='data path')
     parser.add_argument('--output_path', type=str, required=True, help='')
-    parser.add_argument('--pkl', type=str, required=True, help='')
+    parser.add_argument('--pkl_path', type=str, required=True, help='')
     # parser.add_argument('--rdim', '-R', type=int, default=10, help='desired reduced dimension')
     # parser.add_argument('--clf_as_view', '-cav', type=str2bool, default='false', help='desired reduced dimension')
     args = parser.parse_args()
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     node_list = list(G.nodes())
     protein_list = pd.read_csv(os.path.join(args.deepnf_path, 'gene_list.txt'))
 
-    with open(os.path.join(args.deepnf_path, args.pkl), 'rb') as handle:
+    with open(args.pkl, 'rb') as handle:
         b = pickle.load(handle)
     print(b)
     print(type(b))
