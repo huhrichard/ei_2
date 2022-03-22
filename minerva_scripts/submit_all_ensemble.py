83699001cb6bf260da8772a0fd7f5006a9610ec4
Richard <richardpokard@gmail.com>
2022-03-22 09:35:35 -0400

    no message

--- minerva_scripts/submit_all_ensemble.py
+++ minerva_scripts/submit_all_ensemble.py
@@ -6,7 +6,7 @@ script = open(lsf_fn, 'w')
 script.write('#!/bin/bash\n#BSUB -J ensemble_all\n#BSUB -P acc_pandeg01a\n#BSUB -q express\n#BSUB -n 1\n#BSUB -W 10:00\n#BSUB -o ensemble_all.stdout\n#BSUB -eo ensemble_all.stderr\n#BSUB -R rusage[mem=10000]\n')
 script.write('module purge\nmodule load java\nmodule load python\nmodule load groovy\nmodule load selfsched\n')
 
-python_cmd = 'python minerva_script/run_all_different_data_ensemble.py --path {}'.format(ontology)
+python_cmd = 'python minerva_scripts/run_all_different_data_ensemble.py --path {}'.format(ontology)
 script.write(python_cmd)
 script.close()
 os.system('bsub < {}'.format(lsf_fn))
