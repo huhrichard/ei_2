955876ac8af1eaac472e8ca3f2abb87072e3e556
Richard <richardpokard@gmail.com>
2022-03-23 14:23:39 -0400

    no message

--- minerva_scripts/performance_comparison_all.py
+++ minerva_scripts/performance_comparison_all.py
@@ -18,12 +18,12 @@ list_ontology = [
                  # 'hpo'
                 ]
 # python_cmd = 'python performance_comparison.py {} {} {}'
-python_cmd = 'python performance_comparison.py --prepro_path {} --ontology {} --group {} --file_prefix {}'
+python_cmd = 'python minerva_scripts/performance_comparison.py --prepro_path {} --ontology {} --group {} --file_prefix {}'
 system('module load R')
 
 for o in list_ontology:
     for key, val in dict_to_compare.items():
-        cmd = python_cmd.format('../oracle_data/go_string_2022/GO_STRING_2022/preprocessed/',
+        cmd = python_cmd.format('../../oracle_data/go_string_2022/GO_STRING_2022/preprocessed/',
                                 o, key, dir_prefix + val + o)
         # cmd = python_cmd.format(o, key, dir_prefix+val+o)
         print(cmd)
