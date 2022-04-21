c527d341d05f247d756d0a4ce6fb221842f99509
Richard <richardpokard@gmail.com>
2022-04-21 07:21:37 -0400

    no message

--- minerva_scripts/plot_from_cd_input.py
+++ minerva_scripts/plot_from_cd_input.py
@@ -94,6 +94,7 @@ for ontology in list_ontology:
 
         for mk, mv in metrics.items():
             csv_fp = cd_csv_path+'cd_csv/cd_input_{}_{}_{}.csv'.format(ontology, group, mk)
+            # csv_fp = cd_csv_path+'cd_csv/cd_input_{}_{}_{}.csv'.format(ontology, group, mk)
             cd_df = pd.read_csv(csv_fp, index_col=0)
             # cd_df = pd.read_csv(csv_fp, index_col=0)
 
@@ -226,7 +227,8 @@ for ontology in list_ontology:
             ax1.set_xlabel('')
             # ax1.set_title(title_name)
             # fig1.savefig('{}f_max_{}_comparison_{}.pdf'.format(plot_dir, ontology, group_fn_suffix), bbox_inches="tight")
-            fig1.savefig('{}{}_{}_comparison.pdf'.format(plot_dir, mk, ontology), bbox_inches="tight")
+            # fig1.savefig('{}{}_{}_comparison.pdf'.format(plot_dir, mk, ontology), bbox_inches="tight")
+            fig1.savefig('{}{}_{}_comparison_{}.png'.format(plot_dir,mk ontology, group), bbox_inches="tight")
             #
             value_count_depth = cd_df['go_depth'].value_counts().to_dict()
             value_count_ic_bin = cd_df['go_ic'].value_counts(bins=5).to_dict()
@@ -279,7 +281,8 @@ for ontology in list_ontology:
 
                 # ax2.set_title(title_name)
                 # fig2.savefig('{}f_max_{}_by_depth_{}.png'.format(plot_dir, ontology, group_fn_suffix), bbox_inches="tight")
-                fig2.savefig('{}{}_{}_by_depth.pdf'.format(plot_dir,mk, ontology), bbox_inches="tight")
+                # fig2.savefig('{}{}_{}_by_depth.pdf'.format(plot_dir,mk, ontology), bbox_inches="tight")
+                fig2.savefig('{}{}_{}_by_depth_{}.png'.format(plot_dir,mk ontology, group), bbox_inches="tight")
 
 
 
@@ -334,5 +337,5 @@ for ontology in list_ontology:
                     ytick.set_fontsize(22)
                 ax3.set_xlabel('Information Content', fontsize=22)
                 # ax3.set_title(title_name)
-                # fig3.savefig('{}f_max_{}_by_ic_{}.png'.format(plot_dir, ontology, group_fn_suffix), bbox_inches="tight")
-                fig3.savefig('{}{}_{}_by_ic.pdf'.format(plot_dir,mk , ontology), bbox_inches="tight")
+                fig3.savefig('{}{}_{}_by_ic_{}.png'.format(plot_dir,mk ontology, group), bbox_inches="tight")
+                # fig3.savefig('{}{}_{}_by_ic.pdf'.format(plot_dir,mk , ontology), bbox_inches="tight")
