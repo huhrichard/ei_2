fa0c5ca4a169229e41de6b4fdaac758b5864524f
Richard <richardpokard@gmail.com>
2022-11-14 11:59:22 -0500

    no message

--- minerva_scripts/run_all_go_subdir_ensemble.py
+++ minerva_scripts/run_all_go_subdir_ensemble.py
@@ -110,16 +110,16 @@ if __name__ == "__main__":
                         # data = go_scratch_dir.split('/')[-1]
                 data_dir = go_scratch_dir.split('/')[-2]
                 # if data_dir.split('_')[-1] == 'EI':
-                if data_dir.split('_')[-1] == 'cls':
+                # if data_dir.split('_')[-1] == 'cls':
                     # p = subprocess.Popen('python tcca_projection.py --path {}'.format(go_dir))
                     # p.wait()
                     # python_cmd_list.append('python tcca_projection.py --path {}'.format(go_dir))
-                    fns = listdir(go_scratch_dir)
-                    fns = [fn for fn in fns if not fn in excluding_folder]
-                    fns = [os.path.join(go_scratch_dir, fn) for fn in fns]
-                    feature_folders = [fn for fn in fns if isdir(fn)]
-                    for f_dir in feature_folders:
-                        jobs_list.append('python ensemble.py --path {}'.format(f_dir))
+                fns = listdir(go_scratch_dir)
+                fns = [fn for fn in fns if not fn in excluding_folder]
+                fns = [os.path.join(go_scratch_dir, fn) for fn in fns]
+                feature_folders = [fn for fn in fns if isdir(fn)]
+                for f_dir in feature_folders:
+                    jobs_list.append('python ensemble.py --path {}'.format(f_dir))
 
                 if args.rank is False:
                     jobs_list.append('python ensemble.py --path {}'.format(go_scratch_dir))
